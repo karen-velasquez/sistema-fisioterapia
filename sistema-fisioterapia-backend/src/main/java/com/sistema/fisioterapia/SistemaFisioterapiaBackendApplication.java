66dd53b3db36c02521170425b772e2ffe646c1e5
karen-velasquez <maritza.karen.flores.velasquez@gmail.com>
2022-09-19 21:41:59 -0400

    backend spring boot

---
+++ sistema-fisioterapia-backend/src/main/java/com/sistema/fisioterapia/SistemaFisioterapiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.fisioterapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFisioterapiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFisioterapiaBackendApplication.class, args);
+	}
+
+}
---
+++ sistema-fisioterapia-backend/src/test/java/com/sistema/fisioterapia/SistemaFisioterapiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.fisioterapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFisioterapiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
